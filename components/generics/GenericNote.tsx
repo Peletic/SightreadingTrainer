c62520d010b9122cdc1bc9d2d8bae5ac45a7f46d
peletic <its.peletic@gmail.com>
2025-08-20 12:39:28 -0700

    minor sizing adjustments

--- components/generics/GenericNote.tsx
+++ components/generics/GenericNote.tsx
@@ -4,13 +4,13 @@ import {musicCharacterMap} from "@/app/resources/asciiMusicCharacterMap";
 // combining stem = &#119141;
 // qnote = &#119135;
 
-const baseOffset = -2;
+const baseOffset = 3;
 const intervalOffset = -8;
 
 const upStemLOffset = -8;
-const upStemTOffset = 19.5;
+const upStemTOffset = 16.5;
 
-const stemYScalar = 1.2
+const stemYScalar = 1.6
 
 const offsetMap = {
     0: {
@@ -29,7 +29,7 @@ export default function GenericNote({note}: { note: Note }) {
     // we will use a negative top margin dict to accomplish our desired mapped offset
     // assume qnote e4 for testing
 
-    return (<div className={`text-black text-[80px] h-full`}
+    return (<div className={`text-black text-[76px] h-full mx-auto`}
                  style={{marginTop: `${offsetMap["0"][note.key]}px`}}>
         {musicCharacterMap.nsQNote.charPlain}
         <span className={"absolute"}
