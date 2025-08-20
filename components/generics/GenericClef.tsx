50d33d0a761a20f781757e1094a49faeb1a525dd
peletic <its.peletic@gmail.com>
2025-08-20 08:25:46 -0700

    addr issue no 3

--- components/generics/GenericClef.tsx
+++ components/generics/GenericClef.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function GenericClef({svg} : {svg: typeof import("*.svg")}) {
     return(<div className={"relative w-fit h-fit"}>
-        <Image height={96} src={svg} alt={"clef"} objectFit={"contain"} className={"mt-[50%] transform-[translateY(-50%)] bg-yellow-300/70"}>
+        <Image height={112} src={svg} alt={"clef"} objectFit={"contain"} className={"mt-[50%] transform-[translateY(-50%)] bg-yellow-300/70"}>
         </Image>
     </div>)
 }
